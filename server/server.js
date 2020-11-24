d99ce6549ec34d41a847d654a9ba4feb41d2dada
Amaì <amai@tin.it>
2020-11-24 13:27:17 +0100

    Initial server side part completed

--- server/server.js
+++ server/server.js
@@ -2,19 +2,16 @@ import config from './../config/config'
 import app from './express'
 import mongoose from 'mongoose'
 
+// Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 mongoose.connection.on('error', () => {
-    throw new Error(`unable to connect to database: ${mongoUri}`)
+  throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
 app.listen(config.port, (err) => {
-    if (err) {
-        console.log(err)
-    }
-    console.info('Server started on port %s.', config.port)
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', config.port)
 })
